7da84eb93394cfba7067b88ad4d71070009371d0
yuriy <lamukhin.yuriy@gmail.com>
2024-07-09 00:19:26 +0300

    сделаны заготовки к таблицам в flyway

---
+++ src/main/resources/db.migration/V001__create_tables.sql
@@ -0,0 +1,40 @@
+CREATE TABLE IF NOT EXISTS drink
+(
+    id               UUID PRIMARY KEY,
+    name             VARCHAR(50),
+    water_gr         INT,
+    milk_gr          INT,
+    coffee_bean_gr   INT
+);
+
+CREATE TABLE IF NOT EXISTS order_history
+(
+    id               UUID PRIMARY KEY,
+    drink_id         UUID,
+    FOREIGN KEY (drink_id) REFERENCES drink(id),
+    timestamp        TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS coffee_machine_state
+(
+    id                  UUID PRIMARY KEY,
+    factory_number      VARCHAR(50),
+    no_water            BOOLEAN,
+    no_milk             BOOLEAN,
+    no_beans            BOOLEAN,
+    cleaning_required   BOOLEAN,
+    tray_full           BOOLEAN,
+    repair_required     BOOLEAN
+);
+
+CREATE TABLE IF NOT EXISTS usage_counters
+(
+    id                  UUID PRIMARY KEY,
+    coffmach_fact_numb_id      VARCHAR(50),
+    FOREIGN KEY (coffmach_fact_numb_id) REFERENCES coffee_machine_state(id),
+    water_available_gr  INT,
+    milk_available_gr   INT,
+    beans_available_gr   INT,
+    drink_made_counter  INT,
+    liquid_in_tray_gr   INT
+);
